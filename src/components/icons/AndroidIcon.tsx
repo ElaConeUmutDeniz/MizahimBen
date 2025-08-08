d1b33e593b72254ff9e4399877a84c311daa836e
ElaCone <68480298+ElaConeUmutDeniz@users.noreply.github.com>
2025-08-08 12:57:23 +0300

    Create AndroidIcon.tsx

---
+++ src/components/icons/AndroidIcon.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AndroidIcon: React.FC = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="
+            M16 11.5a4.5 4.5 0 00-8 0
+            M10.5 10h.01M13.5 10h.01
+            M9.5 8.5L7.5 6.5M14.5 8.5L16.5 6.5
+            M8 12.5v5h8v-5
+            M6 13v3M18 13v3
+            M10.5 17.5v3M13.5 17.5v3
+        " />
+    </svg>
+);
+
+export default AndroidIcon;
