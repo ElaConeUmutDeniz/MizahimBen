f91d42768c1e340a29a1e0b0657f8a37d7149363
ElaCone <68480298+ElaConeUmutDeniz@users.noreply.github.com>
2025-08-17 00:27:31 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ const MainContent: React.FC = () => {
                 return (
                     <div>
                         <h1 className="text-3xl font-bold mb-6 text-[color:var(--accent-color)]">{t('currentJokes')}</h1>
-                <button className='ml-onclick-form flex items-center space-x-2 px-3 py-2 rounded-md text-sm font-medium transition-colors bg-[color:var(--accent-color)] text-white' onClick={() => (window as any).ml('show', 'NK0MZI', true)}">{t('newsletter')}</button>
+                <button className='ml-onclick-form flex items-center space-x-2 px-3 py-2 rounded-md text-sm font-medium transition-colors bg-[color:var(--accent-color)] text-white' onClick={() => (window as any).ml('show', 'NK0MZI', true)}"> {t('newsletter')}</button>
                         <div className="space-y-6">
                             {jokeSource?.simdiki.map((url, index) => <JokeCard key={url + index} jokeUrl={url} />)}
                         </div>
