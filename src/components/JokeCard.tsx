95c262153dab6089bb606fcce6e1f5e6bb4ab75e
ElaCone <68480298+ElaConeUmutDeniz@users.noreply.github.com>
2026-01-11 21:10:22 +0300

    Delete contact form from JokeCard
    Removed contact form from JokeCard component.

--- src/components/JokeCard.tsx
+++ src/components/JokeCard.tsx
@@ -69,23 +69,6 @@ const JokeCard: React.FC<JokeCardProps> = ({ jokeUrl }) => {
                 >
                     {t('translate')}
                 </a>
-
-
-
-<form name="contact" netlify>
-  <p>
-    <label>Name <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Email <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
-
-
-                
    </div>
         </div>
     );
