69e39c5501cb901c624f22a1d7954c75c57efd3d
ElaCone <68480298+ElaConeUmutDeniz@users.noreply.github.com>
2026-01-12 01:07:51 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -108,6 +108,22 @@ const MainContent: React.FC = () => {
                                 >
                                     {t('newsletter')}
                                 </button>
+                        <script src="https://giscus.app/client.js"
+        data-repo="ElaConeUmutDeniz/MizahimBen"
+        data-repo-id="R_kgDOO076iA"
+        data-category="[KATEGORİ İSMİ GİRİN]"
+        data-category-id="[KATEGORİ ID GİRİN]"
+        data-mapping="specific"
+        data-term="[TAKIM GİRİN]"
+        data-strict="0"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="bottom"
+        data-theme="noborder_dark"
+        data-lang="tr"
+        crossorigin="anonymous"
+        async>
+</script>
                             )}
                         </div>
                         <div
